8afcb30a5d975700a8d769c77123cd3074f7f272
Jprogram01 <113635286+Jprogram01@users.noreply.github.com>
2022-12-04 21:38:19 -0700

    Level 2 Fix
    Gray  Walls previously would not allow for the level to be cleared

--- Level 2.cs
+++ Level 2.cs
@@ -5,4 +5,22 @@ public Level2()
 {
     DictionaryLoader(2);
 }
+public bool NextLevelChecker()
+{
+    int LinesCleared = 0;
+    bool Cleared = false;
+    for (int i = 0; i < BlockDictionary.Count(); i++)
+    {
+        if(BlockDictionary[i].Count == 4)
+        {   
+            LinesCleared += 1;
+            Console.WriteLine(LinesCleared);
+            if (LinesCleared == 8)
+            {
+            Cleared = true;
+            }
+        }
+    }
+    return Cleared;
+}
 }
